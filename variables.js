cf9c766218d0adfe44b6f00e30852df5ee7c8b59
Kiran Kadam <kiran.kadam@qed42.com>
2018-11-01 15:15:49 +0530

    Divider Style Updates. (#55)
    * Divider Style Updates.
    
    * Requested changes: divider element added instead of classes.
    
    * Resolve css linting errors.
    
    * Add @todo comment to system-admin.css

--- variables.js
+++ variables.js
@@ -4,6 +4,7 @@ module.exports = {
     '--color-fg': '#333',
     '--color-bg': '#fff',
     '--color-link': '#0074bd',
+    '--color-divider': 'rgba(142, 146, 156, 0.4)',
     // Typography.
     '--font-family': '\"Lucida Grande\", \"Lucida Sans Unicode\", \"DejaVu Sans\", \"Lucida Sans\", sans-serif'
   }
