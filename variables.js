06522ed03624755746eec73e039f4d140de1cd19
Kiran Kadam <kiran.kadam@qed42.com>
2018-11-01 19:51:02 +0530

    Table Sort Indicator Style Update.

--- variables.js
+++ variables.js
@@ -5,6 +5,7 @@ module.exports = {
     '--color-bg': '#fff',
     '--color-link': '#0074bd',
     '--color-divider': 'rgba(142, 146, 156, 0.4)',
+    '--color-blue': '#004ADC',
     // Typography.
     '--font-family': '\"Lucida Grande\", \"Lucida Sans Unicode\", \"DejaVu Sans\", \"Lucida Sans\", sans-serif'
   }
