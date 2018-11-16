c5a8bb70dde8076600a6af04cb947c9506ca0550
Zoltan Horvath <zoltan.horvath@cheppers.com>
2018-11-16 18:30:58 +0100

    Issue #15 #19: Style input elements directly rather than using pseudos

--- variables.js
+++ variables.js
@@ -52,8 +52,8 @@ module.exports = {
     '--color-input-border-error': 'var(--color-maximumred)',
     '--color-input-label-disabled': 'rgba(84, 85, 96, 0.6)', // Davy's grey with opacity.
     '--color-input-fg-disabled': 'var(--color-oldsilver)',
-    '--color-input-bg-disabled': 'rgba(212, 212, 216, 0.3)', // Light gray with opacity.
-    '--color-input-border-disabled': 'rgba(130, 130, 140, 0.5)', // Old silver with opacity.
+    '--color-input-bg-disabled': 'rgba(212, 212, 216, 0.3)', // Light gray with 0.3 opacity .
+    '--color-input-border-disabled': '#bababf', // Old silver with 0.5 opacity on white bg.
     '--opacity-input-border-disabled': '0.5',
     '--size-input-border-radius': '0.125em', // (1/8)em ~ 2px
     '--size-input-border': '0.0625em', // (1/16)em ~ 1px
